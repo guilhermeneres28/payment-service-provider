cf232c59d7e96241ca9a43bf580398e7416a6c20
Guilherme Neres <guilhermeneresbsb@gmail.com>
2021-11-29 13:57:27 -0300

    :wrench: Add eslint and setup it
    Add eslint and setup it

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({message: 'Hello app is running'});
-})
+app.get('/', (request, response) => response.json({ message: 'Hello app is running' }));
 
 app.listen(3333);
